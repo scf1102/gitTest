5e61093a4d376a479501eb9f2ac25c965e8052dd
scf <scf@123.com>
2021-09-27 20:39:26 +0800

    master commit2

--- src/main/java/com/hkd/git/GitDemo.java
+++ src/main/java/com/hkd/git/GitDemo.java
@@ -7,5 +7,6 @@ public class GitDemo {
         System.out.println("hello ,git3");
         System.out.println("master ,git");
         System.out.println("hot-fix ,git");
+        System.out.println("push ,git");
     }
 }
